1cd14b9f29121fbe68f0e5594b6382df71a3220d
Gabriel V. Silva <karciel@outlook.com>
2023-05-06 09:46:04 -0300

    Nome Completo

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -3,7 +3,21 @@ public class MinhaClasse {
 
 public static void main (String[] args) {
 
-    System.out.print ("olá turma, sejam bem-vindos!");
+    String primeiroNome = "Gabriel";
+    String segundoNome = "Vasconcellos";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+
+
+
+ 
 
 
 
