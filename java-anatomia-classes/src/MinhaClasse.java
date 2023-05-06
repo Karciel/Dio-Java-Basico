1ee3166cf49e26c9ca91183dbbec5d8f9f152105
Gabriel V. Silva <karciel@outlook.com>
2023-05-05 21:44:13 -0300

    Minha-classe

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -3,7 +3,7 @@ public class MinhaClasse {
 
 public static void main (String[] args) {
 
-    System.out.print ("olá turma, sejam bem-vindos a DIO!");
+    System.out.print ("olá turma, sejam bem-vindos a DIO! Hoje aprenderemos tudo sobre 'classes'");
 
 
 
